7d9f888d3183125a1f53ac0ea8ecf2875e924688
Sergio Jurado Casado <sergiojurado.casado@gmail.com>
2023-01-26 14:38:13 +0100

    feat: add tailwind configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,33 @@ import { defineConfig } from 'astro/config';
 // astro integrations imports
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import compress from "astro-compress";
 
 
 export default defineConfig({
-	site: 'https://helpafrica.netlify.app',
+	// site: 'https://helpafrica.netlify.app',
 	integrations: [
-		mdx( {
-			shikiConfig: {
-				theme: 'dracula',
-				wrap: true,
+		mdx(),
+		sitemap(),
+		tailwind( {
+			config: {
+				applyBaseStyles: true,
+				path: './tailwind.config.cjs'
 			}
 		} ),
-		sitemap()],
+		compress( {
+			css: true,
+			html: true,
+			js: true,
+			svg: true,
+			img: true,
+			pipeline: {
+				fulfilled: true,
+				accomplished: true,
+			}
+		})
+	],
 	outDir: 'dist/',
 	output: 'server',
 	server: {

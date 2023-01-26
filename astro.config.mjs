5548c06ec3ca0a65c80c8fc162e546812138ca71
Sergio Jurado Casado <sergiojurado.casado@gmail.com>
2023-01-26 14:19:55 +0100

    feat: Update tsconfig.json changes and review astro structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 
+// astro integrations imports
+import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
+
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'https://helpafrica.netlify.app',
+	integrations: [
+		mdx( {
+			shikiConfig: {
+				theme: 'dracula',
+				wrap: true,
+			}
+		} ),
+		sitemap()],
+	outDir: 'dist/',
+	output: 'server',
+	server: {
+		port: 4200,
+	}
 });
